cce45ebb5a4bed860ed949fcd981b8e898aa1629
Oli Strik <oliverstrik@gmail.com>
2025-01-19 15:54:33 +0100

    init repo

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "dev shell for nix people";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { flake-utils, nixpkgs, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = import ./shell.nix {
+          inherit pkgs;
+        };
+      });
+}
